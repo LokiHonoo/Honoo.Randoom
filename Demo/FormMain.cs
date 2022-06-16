a26401582b69fc66921865779120342490136415
Loki Honoo <lokihonoo@hotmail.com>
2022-06-17 06:27:57 +0800

    添加项目文件。

---
+++ Demo/FormMain.cs
@@ -0,0 +1,101 @@
+using System.Text;
+
+namespace Demo
+{
+    public partial class FormMain : Form
+    {
+        private Honoo.Randoom _randoom = new Honoo.Randoom();
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonGenerate_Click(object sender, EventArgs e)
+        {
+            this.ListViewResult.Items.Clear();
+            if (RadioButtonMark.Checked)
+            {
+                string mark = this.ComboBoxMark.Text;
+                for (int i = 0; i < this.NumericUpDownGenerateCount.Value; i++)
+                {
+                    try
+                    {
+                        this.ListViewResult.Items.Add(new ListViewItem(new string[] { i.ToString(), _randoom.NextString(mark) }));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString(), string.Empty, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                char token;
+                switch (this.ComboBoxToken.SelectedIndex)
+                {
+                    case 6: token = 'h'; break;
+                    case 5: token = 'M'; break;
+                    case 4: token = 'm'; break;
+                    case 3: token = 'A'; break;
+                    case 2: token = 'a'; break;
+                    case 1: token = 'D'; break;
+                    case 0: token = 'd'; break;
+                    default: token = 'm'; break;
+                }
+                int charCount = (int)this.NumericUpDownCharCount.Value;
+                for (int i = 0; i < this.NumericUpDownGenerateCount.Value; i++)
+                {
+                    this.ListViewResult.Items.Add(new ListViewItem(new string[] { i.ToString(), _randoom.NextString(token, charCount) }));
+                }
+            }
+        }
+
+        private void ButtonMarkDescription_Click(object sender, EventArgs e)
+        {
+            StringBuilder description = new StringBuilder();
+            description.AppendLine("返回一个指定字符范围的随机字符串。");
+            description.AppendLine("返回结果：指定字符范围的随机字符串。");
+            description.AppendLine("字符掩码标记：");
+            description.AppendLine("'d' 阿拉伯数字，不包括字形易混淆的字符。");
+            description.AppendLine("'D' 阿拉伯数字。");
+            description.AppendLine("'a' 英文字母，不包括字形易混淆的字符。");
+            description.AppendLine("'A' 英文字母。");
+            description.AppendLine("'m' 英文字母和阿拉伯数字，不包括字形易混淆的字符。");
+            description.AppendLine("'M' 英文字母和阿拉伯数字。");
+            description.AppendLine("'h' 十六进制字符。");
+            description.AppendLine("'c' 使用自定义字符。需配合 '@' 指示符同时使用。");
+            description.AppendLine("'@' 指示符之后的字符作为自定义字符。需配合 'c' 指示符同时使用。");
+            description.AppendLine("'+' 指示符之后的随机字符转换为大写形式。不影响直接输出指示符 '!...!'。");
+            description.AppendLine("'-' 指示符之后的随机字符转换为小写形式。不影响直接输出指示符 '!...!'。");
+            description.AppendLine("'!...!' 指示符之内的字符直接输出，不作为掩码字符。");
+            description.AppendLine("实例：");
+            description.AppendLine("+mmmmm!-!mmmmm!-!mmmmm!-!mmmmm!-!mmmmm 模拟 Windows 序列号。");
+            description.AppendLine("hhhhhhhh!-!hhhh!-!hhhh!-!hhhh!-!hhhhhhhhhhhh 模拟 GUID。");
+            description.AppendLine("!WPD888-5!DDDD!-!DDDDD!-!DDDDD 模拟 Macromedia 8 序列号。");
+            description.AppendLine("cccccccccccccccccccccccc@ABCabc12345~!@#$%^* 自定义字符。");
+            MessageBox.Show(description.ToString(), string.Empty, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            this.ComboBoxToken.Items.Add("'d' 阿拉伯数字，不包括字形易混淆的字符。");
+            this.ComboBoxToken.Items.Add("'D' 阿拉伯数字。");
+            this.ComboBoxToken.Items.Add("'a' 英文字母，不包括字形易混淆的字符。");
+            this.ComboBoxToken.Items.Add("'A' 英文字母。");
+            this.ComboBoxToken.Items.Add("'m' 英文字母和阿拉伯数字，不包括字形易混淆的字符。");
+            this.ComboBoxToken.Items.Add("'M' 英文字母和阿拉伯数字。");
+            this.ComboBoxToken.Items.Add("'h' 十六进制字符。");
+            this.ComboBoxToken.SelectedIndex = 4;
+            this.ComboBoxMark.Items.Add("+mmmmm!-!mmmmm!-!mmmmm!-!mmmmm!-!mmmmm");
+            this.ComboBoxMark.Items.Add("hhhhhhhh!-!hhhh!-!hhhh!-!hhhh!-!hhhhhhhhhhhh");
+            this.ComboBoxMark.Items.Add("!WPD888-5!DDDD!-!DDDDD!-!DDDDD");
+            this.ComboBoxMark.Items.Add("cccccccccccccccccccccccc@ABCabc12345~!@#$%^*");
+        }
+
+        private void ListViewResult_ItemActivate(object sender, EventArgs e)
+        {
+            Clipboard.SetText((sender as ListView).SelectedItems[0].SubItems[1].Text);
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,99 @@
+﻿using Honoo;
+using System;
+using System.Diagnostics;
+
+namespace test
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("=======================================================================================");
+                Console.WriteLine();
+                Console.WriteLine("                       Honoo.Randoom TEST   runtime " + Environment.Version);
+                Console.WriteLine();
+                Console.WriteLine("=======================================================================================");
+
+                Random random = new Random();
+                Randoom randoom = new Randoom();
+                Console.WriteLine("Randoom.NextDouble()");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(randoom.NextDouble());
+                }
+                Console.WriteLine();
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(random.NextDouble());
+                }
+                Console.WriteLine();
+                Console.WriteLine("Randoom.Next()");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(randoom.Next());
+                }
+                Console.WriteLine();
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(random.Next());
+                }
+                Console.WriteLine();
+                Console.WriteLine("Randoom.Next(90000)");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(randoom.Next(90000));
+                }
+                Console.WriteLine();
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(random.Next(90000));
+                }
+                Console.WriteLine();
+                Console.WriteLine("Randoom.Next(-30,30)");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(randoom.Next(-30,30));
+                }
+                Console.WriteLine();
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(random.Next(-30, 30));
+                }
+                Console.WriteLine();
+                Console.WriteLine("Randoom.NextString('m',60)");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(randoom.NextString('M', 60));
+                }
+                Console.WriteLine();
+                Console.WriteLine("Randoom.NextString(\"+mmmmm!-!mmmmm!-!mmmmm!-!mmmmm!-!mmmmm\")");
+                Console.WriteLine(randoom.NextString("+mmmmm!-!mmmmm!-!mmmmm!-!mmmmm!-!mmmmm"));
+                Console.WriteLine(randoom.NextString("+mmmmm!-!mmmmm!-!mmmmm!-!mmmmm!-!mmmmm"));
+                Console.WriteLine(randoom.NextString("+mmmmm!-!mmmmm!-!mmmmm!-!mmmmm!-!mmmmm"));
+                Console.WriteLine();
+                Console.WriteLine("Randoom.NextString(\"hhhhhhhh!-!hhhh!-!hhhh!-!hhhh!-!hhhhhhhhhhhh\")");
+                Console.WriteLine(randoom.NextString("hhhhhhhh!-!hhhh!-!hhhh!-!hhhh!-!hhhhhhhhhhhh"));
+                Console.WriteLine(randoom.NextString("hhhhhhhh!-!hhhh!-!hhhh!-!hhhh!-!hhhhhhhhhhhh"));
+                Console.WriteLine(randoom.NextString("hhhhhhhh!-!hhhh!-!hhhh!-!hhhh!-!hhhhhhhhhhhh"));
+                Console.WriteLine();
+                Console.WriteLine("Randoom.NextString(\"!WPD888-5!DDDD!-!DDDDD!-!DDDDD\")");
+                Console.WriteLine(randoom.NextString("!WPD888-5!DDDD!-!DDDDD!-!DDDDD"));
+                Console.WriteLine(randoom.NextString("!WPD888-5!DDDD!-!DDDDD!-!DDDDD"));
+                Console.WriteLine(randoom.NextString("!WPD888-5!DDDD!-!DDDDD!-!DDDDD"));
+                Console.WriteLine();
+                Console.WriteLine("Randoom.NextString(\"cccccccccccccccccccccccc@ABCabc12345~!@#$%^*\")");
+                Console.WriteLine(randoom.NextString("cccccccccccccccccccccccc@ABCabc12345~!@#$%^*"));
+                Console.WriteLine(randoom.NextString("cccccccccccccccccccccccc@ABCabc12345~!@#$%^*"));
+                Console.WriteLine(randoom.NextString("cccccccccccccccccccccccc@ABCabc12345~!@#$%^*"));
+                Console.WriteLine();
+                Console.ReadKey(true);
+            }
+
+
+
+        }
+    }
+}
